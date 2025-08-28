463f6f0ebc4432b0b78d0133c08a9ccdc0a0909a
3pm-on-github <pookiemon2019@gmail.com>
2025-08-28 12:47:42 +0200

    rivièremc9

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def download_audio(url):
 class XixoBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.msgcount = 3170
+        self.msgcount = 3240
+        self.laccount = 0
         self.defaultmsg = ["so true", "peak", "would YOU do this for 40 yen?", "https://cdn.discordapp.com/attachments/1251355055139852309/1385089077392445551/togif.gif", "and alexander wept, seeing as he had no more worlds to conquer", "eat the rich", "they turned xixo woke!!", "*hic*", "trans rights btw", "3pm's ip address is 104.26.-", "this genuenily seagulls", "this would kill a victorian child", "its beautiful", "i do my best", "86 mahi mahi am i right", "these birds are pissing me off", "im the original                  xixobot", "is that pikachu?", "did u guys hear trump died", "you can leave me a tip right on this laptop!", "bro really wants us to think theyre funny", "brian look out noo", "did you know 90% of my viewers arent subscribed", "no", "yeah", "old", "say cheese", "you can say that again", "should i go visit them? they live 5 mins away from my shoot,", "the glorious german flag: :flag_ge:", "Look ! this man is going for a world record. 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, But Watch out if this guy misses he'll die on the spot. Or he will hurt himself very, very badly. And ALL THIS JUST FOR YOU. Just for your EYES. Just to make this video GOES VIRAL. Will he do it?! WILL HE SUCCEED? That's the question we are asking ourself right now. Look at him ! he's flying he's gliding his flying like a rocket. INCREDIBLE ! This man deserves respect ! You should give him strenght in the comments Check him out ! After nearly breaking his neck, he decided to stop. 😼", 
                           "my sleepy ass could never", "i dont wanna say what im thinking right now", "bro i did not expect that", "shut up and take my money", "they may not be pregnant but they never fail to deliver", "mrrp meoww", "im toby fox creator of undertale", "when you see it youll shit bricks", "heres my amazing protein cupcake recipe! first you take 500 grams of cottage cheese", "you deserve a medal for that one", "alone at the edge of a universe humming a tune", "also try reactbot", "youre bald", "gatorade baby", ":x:", ":white_check_mark:", "i support the death penalty", "what if instead of xixo it was mojo and it was extremely inactive", "i dont believe in magic", "isnt it so funny that a person will eat when theyre hungry but will duck if you throw an apple at their face", f"you rolled a {random.randint(0, 7)}!", "conduite accompagnée :fire:", "crazy? i was crazy once, they locked me in a room. a rubber room with rats. and rats make me crazy", "did you know? R74n moderation is quick, efficient and fair. the french monarchy also said that about themselves and look what happened.", "you won!!!! your new balance is [505](<https://www.youtube.com/watch?v=qU9mHegkTc4>)", "do NOT gamble your xixoyens in evil mode at 3AM :scream:!!!!! (GONE WRONG)", "AND FERRARI DOES NOT WIN THE XIXO GRAND PRIX", "you should watch ratatouille again", "EVIL XIXOBOT SHALL PREVAIL",
                            "is that the guy from fortnite?", "im xixobot!", "wake up", "uhuh", "i remember that one time when a fellow sapling did not capitalize the R in R74n and ryan ordered the mods to execute them with the firing squad", "thats actually really funny", "i burned 3 houses in alabama in monday november 13th at 7 o clock", "*fucking explodes*", "you guys ever try natural ketchup", "as an ai language model i am unable to react to this", "im gonna need a mini xixobot for this one! \n-# this sucks!",
@@ -52,13 +53,13 @@ class XixoBot(discord.Client):
         if message.author == self.user:
             return
         self.msgcount += 1
+        if "lac" in message.content:
+            self.laccount += 1
         self.messages.append(message.content)
         guild = self.get_guild(1409280301666013286)
         if re.search(r"(paw|me+o+w|mrr+)", message.content) and ("<@&1409284344039870484>" in message.content or f"<@{self.user.id}>" in message.content):
             await message.channel.send(random.choice(self.sillymsg))
         if guild:
-            if self.msgcount == 0:
-                self.supersilly = False
             if self.msgcount % 1000 == 0:
                 channel = guild.get_channel(1409280302727303271)
                 if channel:
@@ -96,6 +97,16 @@ class XixoBot(discord.Client):
                     else:
                         await channel.send('ping @everyone ' + str(self.msgcount) + " messages!")
                         print("ping!", self.msgcount, "messages!")
+            if self.laccount == 5:
+                self.laccount = 0
+                channel = guild.get_channel(1409280302727303271)
+                if channel:
+                    if self.evilmode:
+                        await channel.send('EVIL RIVIÈRE!!!!')
+                        print("EVIL RIVIÈRE!!!!")
+                    else:
+                        await channel.send('rivière')
+                        print("rivière")
 
 class XixoBank:
     def __init__(self, xixobankfile, signature):
